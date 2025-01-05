cf89df661821cc9becbf4966c47448fe55f9de51
Umar-Mahmood <Umarmahmood637@gmail.com>
2025-01-05 21:47:40 +0900

    added the top navbar

--- homepage.js
+++ homepage.js
@@ -2,17 +2,17 @@ const hour = new Date().getHours();
 let backgroundImage = "";
 
 if (hour >= 6 && hour < 9) {
-  backgroundImage = 'url("./backgrounds/macOS-Big-Sur-Morning.jpg")';
+  backgroundImage = 'url("./assets/macOS-Big-Sur-Morning.jpg")';
 } else if (hour >= 9 && hour < 12) {
-  backgroundImage = 'url("./backgrounds/macOS-Big-Sur-After-Dawn.jpg")';
+  backgroundImage = 'url("./assets/macOS-Big-Sur-After-Dawn.jpg")';
 } else if (hour >= 12 && hour < 18) {
-  backgroundImage = 'url("./backgrounds/macOS-Big-Sur-Day.jpg")';
+  backgroundImage = 'url("./assets/macOS-Big-Sur-Day.jpg")';
 } else if (hour >= 18 && hour < 21) {
-  backgroundImage = 'url("./backgrounds/macOS-Big-Sur-Evening.jpg")';
+  backgroundImage = 'url("./assets/macOS-Big-Sur-Evening.jpg")';
 } else if (hour >= 21 && hour < 24) {
-  backgroundImage = 'url("./backgrounds/macOS-Big-Sur-Night.jpg")';
+  backgroundImage = 'url("./assets/macOS-Big-Sur-Night.jpg")';
 } else {
-  backgroundImage = 'url("./backgrounds/macOS-Big-Sur-Dawn.jpg")';
+  backgroundImage = 'url("./assets/macOS-Big-Sur-Dawn.jpg")';
 }
 
 document.body.style.backgroundImage = backgroundImage;
